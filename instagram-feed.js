cf62f4f93bd34caa7ec5b547ec222de78205036c
bonialeart <TuCorreo@ejemplo.com>
2025-02-10 01:01:28 -0400

    cambios en el feed de instagram

---
+++ instagram-feed.js
@@ -0,0 +1,12 @@
+<!-- Opción 1: Widget básico de Instagram -->
+<iframe src="https://www.instagram.com/tu_usuario/embed" 
+        width="400" 
+        height="480" 
+        frameborder="0" 
+        scrolling="no" 
+        allowtransparency="true">
+</iframe>
+
+<!-- Opción 2: Feed de Instagram usando elfsight (requiere cuenta) -->
+<script src="https://apps.elfsight.com/p/platform.js" defer></script>
+<div class="elfsight-app-[TU-ID-DE-APP]"></div>
